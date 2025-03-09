c47cd234444da5fdceff0db18d5daf89e895ddbc
alanscode <alnngyn@gmail.com>
2025-03-08 18:29:13 -0800

    Replace HomePage with ChatInterface in App component

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './components/HomePage';
+import ChatInterface from './components/ChatInterface';
 import Header from './components/Header';
 
 const App: React.FC = () => {
@@ -7,7 +7,7 @@ const App: React.FC = () => {
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
       <main className="flex-1 container mx-auto px-4 py-4">
-        <HomePage />
+        <ChatInterface />
       </main>
       <footer className="bg-white py-4 border-t border-gray-200">
         <div className="container mx-auto px-4 text-center text-gray-500 text-sm">
